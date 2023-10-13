996e06a2e455c4002e716be3dfa46bbeaad68007
studycris <147834910+studycris@users.noreply.github.com>
2023-10-13 11:28:52 -0300

    Add files via upload

---
+++ actividad.py
@@ -0,0 +1,48 @@
+precio=0
+cantidad=0
+menu=""
+nombrecliente=""
+
+
+menu=int(input('''
+1.Catán $29990
+2.Dixit $19990
+3.Exploding Kittens $19990
+4.salir
+/nelija su eleccion'''))
+
+if menu=="1":
+ print("usted elijio ocatan,{menu}")
+precio=29990
+elif menu=="1"
+print("elijar otras opciones")
+
+if menu=="2":
+ print("usted elijio dixit.{menu}")
+precio=19990
+elif:
+print("elijar otras opciones")
+
+
+if menu=="3":
+ print("usted elijio ocatan,{menu}")
+precio=29990
+elif
+print("elijar otras opciones"):
+
+if menu=="4":
+    print("gracia por la compra")
+    
+    cantidad=precio+cantidad
+    
+print("------------boleto----------")
+boleto=int(input('''
+Boleta Tienda
+Juegos
+nombrecliente"_____________________"
+1.- Catán $29990
+2.- Dixit $19990
+3.- Exploding Kittens $19990
+4.-salir
+/n elija opciones'''))
+
